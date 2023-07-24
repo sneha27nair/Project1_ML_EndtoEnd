42a47d3cebe478b82d6d071e822efdad928f0a22
Sneha Nair <sneha27nair@gmail.com>
2023-07-24 07:18:05 +0530

    data transformation

--- src/exception.py
+++ src/exception.py
@@ -13,7 +13,7 @@ def error_message_detail(error,error_detail:sys):
     return error_message
 
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys) -> None:
+    def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message,error_detail=error_detail)
     
@@ -21,13 +21,6 @@ class CustomException(Exception):
         return self.error_message
     
 
-if __name__=="__main__":
-
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by zero")
-        raise CustomException(e,sys)
 
 
         
